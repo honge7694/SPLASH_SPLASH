55db2938dfe668a25ae10642296fe7fc00e52cb4
hongui <honge7694@naver.com>
2023-01-27 00:31:51 +0900

    backend 초기 세팅

---
+++ backend/config/settings/dev.py
@@ -0,0 +1,15 @@
+from .common import *
+
+
+# common.py INSTALLED_APPS 맨 뒤에 추가.
+INSTALLED_APPS += [
+    'debug_toolbar',
+    'drf_yasg',
+]
+
+# common.py MIDDLEWARE 맨 앞에 추가.
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+] + MIDDLEWARE
+
+INTERNAL_IPS = ["127.0.0.1"]
