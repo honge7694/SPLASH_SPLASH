09ffd0b6b134796965a101ec326b089909c6d24b
hongui <honge7694@naver.com>
2023-04-21 23:09:33 +0900

    chat model 생성

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class Chat(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
