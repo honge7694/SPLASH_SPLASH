97edb4f6c8a192937bb996749cfde110f39b4944
hongui <honge7694@naver.com>
2023-05-23 00:47:05 +0900

    nginx, uwsgi settings

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -4,8 +4,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 django_asgi_app = get_asgi_application()
